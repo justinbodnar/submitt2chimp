f19d35a04f5d16e24288dc535d538780786e0d60
Justin Bodnar <justinbodnar@users.noreply.github.com>
2017-06-29 15:03:07 -0400

    Update submitt2chimp.py
    Added code to retrieve the email addresses from the last 200 submissions into the Submittable account.

--- submitt2chimp.py
+++ submitt2chimp.py
@@ -1,21 +1,27 @@
+# -*- coding: utf-8 -*-
+from submittable_api_client.submittable_api_client import SubmittableAPIClient
+
 ##################
 # basic workflow #
 
-
 # API information
+SubmittableAPIToken = 'REMOVED'
+Client = SubmittableAPIClient(username="books@etruscanpress.org", apitoken=SubmittableAPIToken)
 
-submittableURL = "https://api.submittable.com/v1/submitters?page=&count="
-submittableAPItoken = 0
-mailchimpAPItoken = 0
+# get list of submitters
+submitters = Client.submitters(page=1, per_page=200)
+print( str( submitters.count ) + " submitters loaded into memory" )
 
+#print list of emails
+for item in submitters.items:
+    print( item.email )
 
-# get list of recent submitters
-
-# get mailing list
+# get MailChimp mailing list for comparison
 
 # for each in recent submitters list
   # check if email exists in mailing list
   # if it does not exist
     # add to email list
-    
+
 # wait (?)
+# we could always just set up a cron job
