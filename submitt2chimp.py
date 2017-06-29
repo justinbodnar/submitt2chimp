7e28d8153b9aee1ff87794a0d867f959a873f369
Justin Bodnar <justinbodnar@users.noreply.github.com>
2017-06-29 12:50:23 -0400

    Create submitt2chimp.py

---
+++ submitt2chimp.py
@@ -0,0 +1,19 @@
+##################
+# basic workflow #
+
+
+# API information
+
+submittableAPItoken = 0
+mailchimpAPItoken = 0
+
+# get list of recent submitters
+
+# get mailing list
+
+# for each in recent submitters list
+  # check if email exists in mailing list
+  # if it does not exist
+    # add to email list
+    
+# wait (?)
