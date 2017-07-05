fb4c5d45ebbd6efa1c11124dc3a18a66911df2a0
Justin Bodnar <justinbodnar@users.noreply.github.com>
2017-07-05 13:32:00 -0400

    Update submitt2chimp.py
    Added use of config file for storing API information.

--- submitt2chimp.py
+++ submitt2chimp.py
@@ -1,23 +1,28 @@
 # -*- coding: utf-8 -*-
-from submittable_api_client.submittable_api_client import SubmittableAPIClient
 
-##################
-# basic workflow #
+
+from submittable_api_client.submittable_api_client import SubmittableAPIClient
+from mailchimp3 import MailChimp
+import config as cfg
 
 # API information
-SubmittableAPIToken = 'REMOVED'
-SubmittableUsername = 'REMOVED'
-Client = SubmittableAPIClient(username=SubmittableUsername, apitoken=SubmittableAPIToken)
+
+SClient = SubmittableAPIClient(username=cfg.SubmittableUsername, apitoken=cfg.SubmittableAPIToken)
+MCClient = MailChimp(cfg.MailChimpUsername, cfg.MailChimpAPIToken)
+
 
 # get list of submitters
-submitters = Client.submitters(page=1, per_page=200)
-print( str( submitters.count ) + " submitters loaded into memory" )
+#submitters = Client.submitters(page=1, per_page=200)
+#print( str( submitters.count ) + " submitters loaded into memory" )
 
 #print list of emails
-for item in submitters.items:
-    print( item.email )
+#for item in submitters.items:
+#    print( item.email )
 
 # get MailChimp mailing list for comparison
+
+for each in MCClient.lists.members.all(cfg.MailChimpMailingListID, get_all=True, fields="members.email_address")['members']:
+	print( each )
 
 # for each in recent submitters list
   # check if email exists in mailing list
@@ -25,4 +30,3 @@ for item in submitters.items:
     # add to email list
 
 # wait (?)
-# we could always just set up a cron job
