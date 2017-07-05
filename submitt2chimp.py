5a10cb5ca9ec93dca7b77ebf46def94d8768238a
Justin Bodnar <justinbodnar@users.noreply.github.com>
2017-07-05 14:45:54 -0400

    Update submitt2chimp.py
    Added use of MailChimp API v2 due to issues with the v3 API python lib's inability to accurately report errors when attempting to add new emails to an existing list.

--- submitt2chimp.py
+++ submitt2chimp.py
@@ -1,32 +1,50 @@
 # -*- coding: utf-8 -*-
 
-
 from submittable_api_client.submittable_api_client import SubmittableAPIClient
 from mailchimp3 import MailChimp
+import mailchimp
 import config as cfg
 
-# API information
-
+# API Client objects
 SClient = SubmittableAPIClient(username=cfg.SubmittableUsername, apitoken=cfg.SubmittableAPIToken)
 MCClient = MailChimp(cfg.MailChimpUsername, cfg.MailChimpAPIToken)
 
-
 # get list of submitters
-SubmittableList = SClient.submitters(page=1, per_page=200)
-print( str( SubmittableList.count ) + " submitters loaded into memory" )
-
-#for item in submitters.items:
-#    print( item.email )
+SubmittableResponse = SClient.submitters(page=1, per_page=200)
+SubmittableList = list()
+for each in SubmittableResponse.items:
+	SubmittableList.append( each.email )
 
 # get MailChimp mailing list for comparison
-MailChimpList = list()
+MailChimpListUnsplit = list()
 for each in MCClient.lists.members.all(cfg.MailChimpMailingListID, get_all=True, fields="members.email_address")['members']:
-	MailChimpList.append( each )
+	MailChimpListUnsplit.append( each )
+# split MailChimpList entries and add to usable list
+MailChimpList = list()
+for entry in MailChimpListUnsplit:
+	temp = str( entry )
+	temp = temp[21:-2]
+	MailChimpList.append( temp )
+
+# print sizes of lists to stdout
+print
 print( str( len( MailChimpList ) ) + " subscribers loaded into memory" )
+print
+print( str( len( SubmittableList ) ) + " submitters loaded into memory" )
 
-# for each in recent submitters list
-  # check if email exists in mailing list
-  # if it does not exist
-    # add to email list
+# loop through submitters and check if they are subscribed to MC list
+i = 0
+for submitter in SubmittableList:
+	# check if email exists in mailing list
+	if submitter.lower() in (subscribed.lower() for subscribed in MailChimpList):
+		print( submitter + " is already subscribed." )
+	else:
+		print( submitter + " is not subscribed." )
+		# add the email, increment counter
+		MCClientv2 = mailchimp.Mailchimp(cfg.MailChimpAPIToken)
+		MCClientv2.lists.subscribe(cfg.MailChimpMailingListID, {'email': submitter})
+		i = i + 1
 
-# wait (?)
+# print report to stdout
+print
+print( str(i) + " new subscribers loaded into MailChimp mailing list." )
